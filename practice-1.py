e72a97887ceebb8e2f1cb101809f323c25c26459
Hanna Podzharova <hanna.podzharova@gmail.com>
2018-07-24 23:24:27 +0200

    MMC applied fix #2

--- practice-1.py
+++ practice-1.py
@@ -11,3 +11,19 @@ def points(n):
     return m
 
 print(points(50))
+
+def check(p):
+    return p[0]**2 + p[1]**2 <= 1
+
+print(check(ranpoint()))
+
+print(list(map(check,points(50))))
+
+N = 500000
+k = 0
+for x in map(check,points(N)):
+    if x: k+=1
+print(k)
+
+print(k/N*4)
+
